347e8bd737079fc6d5a0f9d19b3d20ff3f316e0a
jxbenoit <37383779+jxbenoit@users.noreply.github.com>
2022-07-12 11:32:22 -0700

    First change of main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*
+ * Hello World
+ */
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+    cout << "Hello world! This is some code in a Git repo." << endl;
+    return 0;
+}
+
